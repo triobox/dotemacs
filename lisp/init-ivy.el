623f13dc1fc18f7d2a0bf9f4311315000180855d
Liu <minjie.liu@nokia-sbell.com>
2019-04-10 00:09:43 +0800

    add ace-window package in init-ivy.el

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -142,5 +142,24 @@
   :ensure t
   :init (setq xref-show-xrefs-function #'ivy-xref-show-xrefs))
 
+(use-package ace-window
+  :config
+  (global-set-key (kbd "M-p") 'ace-window)
+  (setq aw-dispatch-always t)
+  ;; (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  (defvar aw-dispatch-alist
+    '((?x aw-delete-window " Ace - Delete Window")
+      (?m aw-swap-window " Ace - Swap Window")
+      (?n aw-flip-window)
+      (?v aw-split-window-vert " Ace - Split Vert Window")
+      (?b aw-split-window-horz " Ace - Split Horz Window")
+      (?i delete-other-windows " Ace - Maximize Window")
+      (?o delete-other-windows))
+    "List of actions for `aw-dispatch-default'.")
+
+  )
+
+
+
 (provide 'init-ivy)
 ;;; init-ivy.el ends here
