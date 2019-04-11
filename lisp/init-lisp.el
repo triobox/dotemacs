14aa22221676841bf697b8a3b22f898a88e1c7eb
Liu <minjie.liu@nokia-sbell.com>
2019-04-12 00:15:05 +0800

    drop linpyville-mode

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -4,8 +4,8 @@
 
 (use-package lispy
   :defer t
-  ;; :diminish (lispy-mode . " λ")
-  :diminish (lispy-mode . "")
+  :diminish (lispy-mode . " λ")
+  ;; :diminish (lispy-mode . "")
   :commands lispy-mode
   ;; :hook ((emacs-lisp-mode-hook lisp-mode-hook) . lispy-mode)
   :init
@@ -14,12 +14,12 @@
   :config
   (setq lispy-avy-keys '(?a ?u ?i ?e ?t ?s ?r ?n ?m)))
 
-(use-package lispyville
-  :after (lispy)
-  :defer t
-  :diminish (lispyville-mode . " λe")
-  :init
-  (add-hook 'lispy-mode-hook #'lispyville-mode))
+;; (use-package lispyville
+;;   :after (lispy)
+;;   :defer t
+;;   :diminish (lispyville-mode . " λe")
+;;   :init
+;;   (add-hook 'lispy-mode-hook #'lispyville-mode))
 
 
 (defun lispy-insert-prev-outline-body ()
