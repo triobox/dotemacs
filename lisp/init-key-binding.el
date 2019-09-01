1510949bf6ba0bc708f7293455c9f1437979a6f1
Liu <minjie.liu@nokia-sbell.com>
2019-09-01 23:57:47 +0800

    add keybinding for toggle menubar

--- lisp/init-key-binding.el
+++ lisp/init-key-binding.el
@@ -6,6 +6,10 @@
 ;; global Key-binding
 ;; -------------------------------------------------------
 (global-set-key (kbd "C-j") 'delete-indentation)
+(global-set-key (kbd "C-x <deletechar>" ) 'backward-kill-sentence)
+
+;; Toggle menubar keybind
+(global-set-key (kbd "C-c m") 'menu-bar-mode)
 
 ;; Make C-x C-v undefined
 ;; (global-unset-key "\C-x\C-v ")
