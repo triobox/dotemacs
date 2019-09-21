c45a2249e2386bdc6bb5b91215c38f0b68644400
lmj <mj@laptop>
2019-09-21 15:39:05 +0800

    bind C-c p /s-p to projectile-mode

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -3,49 +3,44 @@
 ;;; Code:
 
 (use-package projectile
-  :defer t
+  :defer 0.3
   :diminish (projectile-mode . " Prj")
   ;;Remove the mode name for projectile-mode, but show the project name
   ;; :delight '(:eval (concat " " (projectile-project-name)))
-  :bind (("C-c p" . hydra-projectile/body))
+  ;; :bind (("C-c p" . hydra-projectile/body))
+  :init
+  (setq projectile-completion-system 'ivy)
   :config
   (projectile-mode +1)
-  ;; (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  )
 
+;; (use-package ibuffer-projectile :ensure t)
 
-  ;; (use-package ibuffer-projectile :ensure t)
-  (use-package counsel-projectile
-    :defer t
-    :config
-    (counsel-projectile-mode +1))
+(use-package counsel-projectile
+  :defer t
+  :config
+  (counsel-projectile-mode +1))
 
- )
+(defhydra hydra-projectile (:color blue :columns 3)
+  "  Projectile  "
 
-(defhydra hydra-projectile (:color blue)
-  "
-  ^
-  ^Projectile^        ^Buffers^           ^Find^              ^Search^
-  ^──────────^────────^───────^───────────^────^──────────────^──────^────────────
-  _q_ quit            _b_ list            _d_ directory       _r_ replace
-  _i_ reset cache     _K_ kill all        _D_ root            _R_ regexp replace
-  ^^                  _S_ save all        _f_ file            _s_ search
-  ^^                  ^^                  _p_ project         ^^
-  ^^                  ^^                  ^^                  ^^
-  "
-  ("q" nil)
-  ("b" counsel-projectile-switch-to-buffer)
-  ("d" counsel-projectile-find-dir)
-  ("D" projectile-dired)
-  ("f" counsel-projectile-find-file)
-  ("i" projectile-invalidate-cache :color red)
-  ("K" projectile-kill-buffers)
-  ("p" counsel-projectile-switch-project)
-  ("r" projectile-replace)
-  ("R" projectile-replace-regexp)
-  ("s" counsel-projectile-git-grep)
-  ("S" projectile-save-project-buffers))
+  ("o" counsel-projectile "counsel-project")
+  ("b" counsel-projectile-switch-to-buffer "switch-buffer")
+  ("d" counsel-projectile-find-dir "find-dir")
+  ("D" projectile-dired "proj-root")
+  ("f" counsel-projectile-find-file "find-file")
+  ("i" projectile-invalidate-cache "reset-cache" :color red)
+  ("K" projectile-kill-buffers "kill-all")
+  ("p" counsel-projectile-switch-project "switch-project")
+  ("r" projectile-replace "replce")
+  ("R" projectile-replace-regexp "regexp-replace")
+  ("s" counsel-projectile-git-grep "search-git")
+  ("S" projectile-save-project-buffers "save-all")
+  ("q" nil "quit" :color red)
 
-
+  )
 
 (provide 'init-projectile)
 ;;; init-projectile.el ends here
