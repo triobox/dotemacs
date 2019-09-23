9ccb42b1539cbdd9e3c29fe866b50b651bc3aacf
Liu <minjie.liu@nokia-sbell.com>
2019-09-23 22:08:32 +0800

    bind C-x w to ace-window

--- lisp/init-ace-window.el
+++ lisp/init-ace-window.el
@@ -4,23 +4,29 @@
 
 (use-package ace-window
   :diminish (ace-window-mode . " AceW")
+  :bind (("C-x w" . ace-window))
   :config
-  (global-set-key (kbd "C-x w") 'ace-window)
+  ;; (global-set-key (kbd "C-x w") 'ace-window)
   (setq aw-minibuffer-flag t)
   (setq aw-dispatch-always t)
-  ;; (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
-  ;; (defvar aw-dispatch-alist
-  ;;   '((?x aw-delete-window " Ace - Delete Window")
-  ;;     (?m aw-swap-window " Ace - Swap Window")
-  ;;     (?n aw-flip-window)
-  ;;     (?v aw-split-window-vert " Ace - Split Vert Window")
-  ;;     (?b aw-split-window-horz " Ace - Split Horz Window")
-  ;;     (?i delete-other-windows " Ace - Maximize Window")
-  ;;     (?o delete-other-windows)
-  ;;     (?? aw-show-dispatch-help))
-  ;;   "List of actions for `aw-dispatch-default'.")
+  (defvar aw-dispatch-alist
+    '((?x aw-delete-window "Delete Window")
+      (?m aw-swap-window "Swap Windows")
+      (?M aw-move-window "Move Window")
+      (?c aw-copy-window "Copy Window")
+      (?j aw-switch-buffer-in-window "Select Buffer")
+      (?n aw-flip-window)
+      (?u aw-switch-buffer-other-window "Switch Buffer Other Window")
+      (?F aw-split-window-fair "Split Fair Window")
+      (?v aw-split-window-vert "Split Vert Window")
+      (?b aw-split-window-horz "Split Horz Window")
+      (?o delete-other-windows "Delete Other Windows")
+      (?? aw-show-dispatch-help))
+    "List of actions for `aw-dispatch-default'.")
 
   )
+
+
 
 (provide 'init-ace-window)
 ;;; init-ace-window.el ends here
